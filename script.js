188a695931b409fafd31335e9a50824b06905bbc
HICHAM-ELHAIBA <hichame.elhaiba1@gmail.com>
2025-07-20 19:32:18 +0100

    script.js

--- script.js
+++ script.js
@@ -34,9 +34,6 @@ function openModal(id) {
 function closeModal(id) {
   document.getElementById(id + '-modal').style.display = 'none';
 }
-window.openModal = openModal;
-window.closeModal = closeModal;
-
 window.onclick = function(event) {
   const modals = document.querySelectorAll('.modal');
   modals.forEach(modal => {
